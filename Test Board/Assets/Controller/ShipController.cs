46bbca94095aae36953484115c369b8e9ef957d8
Oisín Headen <s3513821@student.rmit.edu.au>
2018-06-18 09:40:18 +1000

    Set up basic map

---
+++ Test Board/Assets/Controller/ShipController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : ScriptableObject 
+{
+    public Space CurrentSpace { get; private set; }
+    public GameObject ship;
+	
+    public static ShipController Create(Space startSpace, GameObject toSpawn)
+    {
+        ShipController ship = CreateInstance<ShipController>();
+        ship.CurrentSpace = startSpace;
+        ship.ship = Instantiate(toSpawn, startSpace.GetPosition(), Quaternion.identity) as GameObject;
+        return ship;
+    }
+
+    public void Move(Space newSpace)
+    {
+        CurrentSpace = newSpace;
+        ship.gameObject.transform.position = newSpace.GetPosition();
+    }
+}
