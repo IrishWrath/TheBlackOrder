33e3df16b459c757ac701cdd2b234454c0059130
Oisín Headen <s3513821@student.rmit.edu.au>
2018-06-18 09:45:28 +1000

    Renamed Project to BlackBeyond

---
+++ BlackBeyond/Assets/Controller/ModelLink.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelLink 
+{
+    private GameObject toSpawn;
+
+    public ModelLink(GameObject toSpawn)
+    {
+        this.toSpawn = toSpawn;
+    }
+    public ISpaceCallback CreateSpaceView(int row, int column)
+    {
+        return SpaceController.Create(row, column, toSpawn);
+    }
+}
