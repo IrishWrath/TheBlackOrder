0e2cdefc8ed28807ab43449b98ed9fd2361aa42b
Oisín Headen <s3513821@student.rmit.edu.au>
2018-06-18 10:29:41 +1000

    Added all files

---
+++ BlackBeyond/Assets/Controller/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour 
+{
+    public Map Map { get; private set; }
+
+    private IModelLink modelLink;
+    public GameObject toSpawn;
+    public GameObject ship;
+    public Material selectColour;
+
+    // Use this for initialization
+    void Start () 
+    {
+        this.modelLink = new ModelLink(toSpawn);
+        this.Map = new Map(5, 5, modelLink);
+
+        ShipController.Create(Map.GetSpace(1,4), ship);
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+}
---
+++ BlackBeyond/Assets/Model/IModelLink.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IModelLink 
+{
+    ISpaceCallback CreateSpaceView(int row, int column);
+}
