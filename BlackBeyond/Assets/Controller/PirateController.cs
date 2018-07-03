ae8661461ca5aa7098c1e818588172980db8413b
Joseph Laferlita <39981952+JosephLaferlita@users.noreply.github.com>
2018-07-03 19:25:47 +1000

    started working on pirate ship stats and different classes of ships

---
+++ BlackBeyond/Assets/Controller/PirateController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+    }
+
+// Update is called once per frame
+void Update () {
+		
+	}
+}
+
+
+
+/*END OF TURN METHOD(will be called at end of turn by the turn structure)
+    If pursuing,
+        Pursue
+    else
+        repeat
+            check for enemies(calling a range function)
+                shoot them
+                start pursuing
+            move to next space on path
+In PatrolPath Model class
+CONSTRUCTOR
+    gets the patrol path from pathfinding
+END OF TURN
+    If the pirate is dead
+        make a new one*/
---
+++ BlackBeyond/Assets/Model/PirateModel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateModel : MonoBehaviour {
+        private PirateController pirateController;
+        private SpaceModel pirateSpaceModel;
+        
+      
+        
+    //ship combat stat variables
+       
+        private int health;
+        private int damage;
+        private int detectRange;
+        private int attackRange;
+        private int maxPirateMovement;
+        public int currentPirateMovement;
+        private int currentMovement;
+
+    //pirate ship builder template
+    public PirateModel(int health, int damage, int detectRange, int attackRange , int maxPirateMovement)
+    {
+     
+        this.health = health;
+        this.damage = damage;
+        this.detectRange = detectRange;
+        this.attackRange = attackRange;
+        this.maxPirateMovement = maxPirateMovement;
+        this.currentPirateMovement = maxPirateMovement;
+    }
+
+    //pirate ship classes and stats
+    PirateModel scout = new PirateModel(2, 1, 3, 1, 4);
+    PirateModel frigate = new PirateModel(4, 2, 3, 2, 3);
+    PirateModel platform = new PirateModel(4, 1, 4, 4, 0);
+    PirateModel destroyer = new PirateModel(7, 2, 3, 2, 2);
+    PirateModel dreadnaught = new PirateModel(10, 4, 2, 1, 2);
+
+    
+
+    public PirateModel(SpaceModel pirateSpace)
+        {
+            this.pirateSpaceModel = pirateSpace;
+        }
+        
+
+        public PirateController GetController()
+        {
+            return pirateController;
+        }
+
+        public void SetController(PirateController controller)
+        {
+            this.pirateController = controller;
+        }
+
+        public SpaceModel GetSpace()
+        {
+            return pirateSpaceModel;
+        }
+
+        public int GetCurrentMovement()
+        {
+            return currentMovement;
+        }
+
+        public void UpdateCurrentMovement(int movementUsed)
+        {
+            currentMovement = currentMovement - movementUsed;
+        }
+    }
