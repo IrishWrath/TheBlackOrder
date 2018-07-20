2f6b7e6687649804f4e60e6ae90c24eef661fe2c
Oisín Headen <s3513821@student.rmit.edu.au>
2018-07-20 13:19:09 +1000

    PiratePlatformTemplate

---
+++ BlackBeyond/Assets/Model/PiratesAI/PlatformAi.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlatformAi : PirateAiModel
+{
+    private SpaceModel location;
+
+    protected PlatformAi(PirateModel.PirateType pirateType, MapModel map, SpaceModel location) : base(pirateType, map)
+    {
+        this.location = location;
+    }
+
+    public override void EndTurn()
+    {
+        // If player in range
+            // Shoot
+    }
+}
