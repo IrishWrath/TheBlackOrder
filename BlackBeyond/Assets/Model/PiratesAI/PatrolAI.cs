4bea7fad1f373969f9d681a7208d6b5e23d69fad
Joseph Laferlita <39981952+JosephLaferlita@users.noreply.github.com>
2018-07-24 22:13:10 +1000

    First attempt patrol path

--- BlackBeyond/Assets/Model/PiratesAI/PatrolAI.cs
+++ BlackBeyond/Assets/Model/PiratesAI/PatrolAI.cs
@@ -4,15 +4,24 @@ using System.Collections.Generic;
 
 public class PatrolAI : PirateAiModel
 {
+    public bool engaged;
     protected PatrolAI(PirateModel.PirateType pirateType, MapModel map, ModelLink modelLink, List<SpaceModel> patrolPoints) : base(pirateType, map, modelLink)
     {
+        //**Oisin is this on the right track?**
+        Vector2 [] position = new Vector2[2];
+        position[0] = new Vector2(Random.Range(10, 15), Random.Range(10, 15));
+        position[1] = new Vector2(Random.Range(14, 20), Random.Range(14, 20));
+        position[2] = new Vector2(Random.Range(19, 25), Random.Range(19, 25));
         // Create Patrol route from patrol points
         // PP 1 -> PP 2 -> PP 3... combined into one list. When it reaches the end, it starts again.
     }
 
     public override void EndTurn()
     {
-        // If Pursuing
+         
+    if (engaged == true){
+            //**trying to set target to players current location, not sure what its not liking.**
+        }SpaceModel target = PirateModel.GetSpace;
             // Pursue
         // else
             // Patrol
