96f16cafb3ccc9318b033ad726b9e6994f3a47c8
SethvirKayn <kjdfowler@gmail.com>
2018-07-24 11:12:45 +0930

    StationModel and StationController

---
+++ BlackBeyond/Assets/Controller/StationController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StationController : MonoBehaviour
+{
+    private StationModel stationModel;
+
+    public void SetModel(StationModel stationModel)
+    {
+        this.stationModel = stationModel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
