d52033741842141561c7a1234d015f0844b24597
Joseph Laferlita <39981952+JosephLaferlita@users.noreply.github.com>
2018-07-27 17:56:49 +1000

    working on pursuing

--- BlackBeyond/Assets/Model/PiratesAI/PatrolAI.cs
+++ BlackBeyond/Assets/Model/PiratesAI/PatrolAI.cs
@@ -4,55 +4,44 @@ using System.Collections.Generic;
 
 public class PatrolAI : PirateAiModel
 {
+    public bool engaged;
+    private MapModel map;
+
     protected PatrolAI(PirateModel.PirateType pirateType, MapModel map, ModelLink modelLink, List<SpaceModel> patrolPoints) : base(pirateType, map, modelLink)
     {
+        List<SpaceModel> wayPoints = new List<SpaceModel>();
+        wayPoints[0] = new SpaceModel(startSpace); //or (ShipModel.GetSpace());
+        wayPoints[1] = new SpaceModel(Random.Range(14, 20), Random.Range(14, 20), map);
+        wayPoints[2] = new SpaceModel(Random.Range(19, 25), Random.Range(19, 25), map);
         // Create Patrol route from patrol points
         // PP 1 -> PP 2 -> PP 3... combined into one list. When it reaches the end, it starts again.
     }
 
     public override void EndTurn()
     {
-        // If Pursuing
-            // Pursue
-        // else
-            // Patrol
+        while (pirateModel.GetCurrentMovement() != 0)
+        {
+            if (engaged == true)
+            {
+                PlayerModel target;
+                {
+                    List<PathfindingNode> fov = Pathfinding.GetFieldOfView(pirateModel.GetSpace(), pirateModel.GetDetectRange() * 3, map);
+                    foreach (PathfindingNode node in fov)
+                    {
+                        if (node.GetSpace().GetPlayer() != null)
+                        {
+                            target = node.GetSpace().GetPlayer();
+                            //insert method to move ship?
+                        }
+                    }
+                    // Outside for loop, no players found
+                    engaged = false;
+                    // Return to original patrol path
+                }
+            }
+
+
+        }
+
     }
-
-
-
-// Moved from PirateController
-    //public void Engagement()
-    //{
-    //    while (pirateModel.GetCurrentMovement() != 0)
-    //    {
-    //        if (engaged == true)
-    //        {
-    //            //pirateShip scans spaces inside detection range
-    //            //is target within detection range, if yes
-    //            //is target within attackRange, attack, reduce movment counter by 1
-
-           // this has been replaced with a shoot function
-    //            int armor = PlayerModel.GetArmor();
-    //            int currentHealth = PlayerModel.GetHealth();
-    //            int shotDamage = pirateModel.GetDamage();
-    //            int adjDamage = armor - shotDamage;
-    //            if (adjDamage <=0)
-    //            {
-    //                adjDamage = 0;
-    //            }
-    //            int remainingHP = currentHealth - adjDamage;
-    //            PlayerModel.UpdatePlayerHealth(remainingHP);
-    //        }
-    //        else
-    //        {
-    //            engaged = false;
-    //            //follow patrol route for 1 movement
-    //            //pirateShip scans spaces inside detection range
-    //            //if target found engaged = true
-
-    //        }
-    //    }
-
-
-    //}
 }
