4a5cfb27da9ee3f4ad295be544e0eaa24f6b5c0d
Oisín Headen <s3513821@student.rmit.edu.au>
2018-08-09 11:03:58 +1000

    Loading Scene

---
+++ BlackBeyond/Assets/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour 
+{
+    bool loadScene = false;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LoadNewScene());
+    }
+
+    IEnumerator LoadNewScene()
+    {
+
+        // This line waits for 3 seconds before executing the next line in the coroutine.
+        // This line is only necessary for this demo. The scenes are so simple that they load too fast to read the "Loading..." text.
+        yield return new WaitForSeconds(3);
+
+        // Start an asynchronous operation to load the scene that was passed to the LoadNewScene coroutine.
+        AsyncOperation async = SceneManager.LoadSceneAsync("MainScene");
+
+        // While the asynchronous operation to load the new scene is not yet complete, continue waiting until it's done.
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+    }
+}
