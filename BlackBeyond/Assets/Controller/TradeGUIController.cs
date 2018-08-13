2979089872e44837221d29345085e4eb3bf19dbf
Oisín Headen <s3513821@student.rmit.edu.au>
2018-08-14 09:02:51 +1000

    saved these

---
+++ BlackBeyond/Assets/Controller/TradeGUIController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TradeGUIController : MonoBehaviour 
+{
+    public GameObject text;
+
+    public GameObject dock;
+
+    public GameObject playerFuel;
+    public GameObject playerIce;
+    public GameObject playerOrganics;
+    public GameObject playerMetal;
+    public GameObject playerGas;
+
+    public GameObject fuelCost;
+    public GameObject iceCost;
+    public GameObject organicsCost;
+    public GameObject metalCost;
+    public GameObject gasCost;
+
+
+    public void ShowUI(int player)
+    {
+        
+    }
+
+    public void BuyFuel()
+    {
+
+    }
+    public void BuyIce()
+    {
+
+    }
+    public void BuyOrganics()
+    {
+
+    }
+    public void BuyMetal()
+    {
+
+    }
+    public void BuyGas()
+    {
+        
+    }
+
+    public void SellIce()
+    {
+
+    }
+    public void SellOrganics()
+    {
+
+    }
+    public void SellMetal()
+    {
+
+    }
+    public void SellGas()
+    {
+
+    }
+
+    public void Undock()
+    {
+        dock.SetActive(false);
+    }
+}
